b2073abafc36b7a80ee27115c2b78b0892153b88
Linesh <linesh.simpcity@gmail.com>
2017-08-12 17:16:39 +0800

    [Linesh] ♻️Refactor: move repeated offsetwidth access outside the loop

--- dist/pizza/js/bundle/main.js
+++ dist/pizza/js/bundle/main.js
@@ -449,10 +449,12 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    var randomPizzaContainer = document.querySelectorAll('.randomPizzaContainer')
+    var dx = determineDx(randomPizzaContainer[0], size);
+    var newWidth = (randomPizzaContainer[0].offsetWidth + dx) + 'px'
+
+    for (var i = 0; i < randomPizzaContainer.length; i++) {
+      randomPizzaContainer[i].style.width = newWidth;
     }
   }
 
