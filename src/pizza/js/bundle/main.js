efe1980c4bc1bb17db8f4f541d8d3024151c9feb
Linesh <linesh.simpcity@gmail.com>
2017-08-14 18:18:20 +0800

    [Linesh] ♻️Refactor: rename variable, have nothing to do with performance

--- src/pizza/js/bundle/main.js
+++ src/pizza/js/bundle/main.js
@@ -547,15 +547,15 @@ window.addEventListener('scroll', updatePositions);
  */
 document.addEventListener('DOMContentLoaded', function() {
   var numberOfPizzasInOneRow = 8;
-  var imageSize = 256;
+  var imageBlockSize = 256;
   for (var i = 0; i < 200; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
     elem.style.height = "100px";
     elem.style.width = "73.333px";
-    elem.style.left = (i % numberOfPizzasInOneRow) * imageSize + 'px';
-    elem.style.top = (Math.floor(i / numberOfPizzasInOneRow) * imageSize) + 'px';
+    elem.style.left = (i % numberOfPizzasInOneRow) * imageBlockSize + 'px';
+    elem.style.top = (Math.floor(i / numberOfPizzasInOneRow) * imageBlockSize) + 'px';
     document.querySelector("#movingPizzas1").appendChild(elem);
   }
   updatePositions();
