0325ce23b6ad8def4fe7a348de0f97f5c40d4914
Linesh <linesh.simpcity@gmail.com>
2017-08-16 09:44:03 +0800

    [Linesh] ♻️Refactor: extract loop irrelevant getElementById() outside

--- dist/pizza/js/bundle/main.js
+++ dist/pizza/js/bundle/main.js
@@ -412,9 +412,9 @@ var pizzaElementGenerator = function(i) {
 
 window.performance.mark('mark_start_generating'); // collect timing data
 
+var pizzasDiv = document.getElementById('randomPizzas');
 // This for-loop actually creates and appends all of the pizzas when the page loads
 for (var i = 2; i < 100; i++) {
-  var pizzasDiv = document.getElementById('randomPizzas');
   pizzasDiv.appendChild(pizzaElementGenerator(i));
 }
 
